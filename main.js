8301b0ad472c93e6f1e6f23207567906ba2a9828
Riya07707 <113397550+Riya07707@users.noreply.github.com>
2023-09-05 20:37:03 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preLoad() {
+
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function draw() {
+
+}
+
+function take_snapshot() {
+    save('My Awesome/Fantastic Picture.png');
+    
+}
